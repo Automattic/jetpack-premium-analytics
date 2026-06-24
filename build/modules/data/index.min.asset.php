6101c7f15a0e84b70b1205449504b85eed59e0cc
kangzj <kangzj@users.noreply.github.com>
2026-06-24 22:29:36 +0000

    Premium Analytics: add Stats app commercial classification endpoint (#49887)
    * Premium Analytics: add Stats app commercial classification endpoint
    
    * Address review: document commercial classification mutation
    
    * Address review: use contextual query client
    
    * Premium Analytics: type commercial classification params
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28133020809
    
    Upstream-Ref: Automattic/jetpack@2f397509a96bc3f5b3fa4d3a0a70532fc7220bde

--- build/modules/data/index.min.asset.php
+++ build/modules/data/index.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-core-data', 'wp-data', 'wp-i18n', 'wp-url'), 'version' => '1ac573e3e5251b38fc9c');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-core-data', 'wp-data', 'wp-i18n', 'wp-url'), 'version' => '3f24d5ab4def13c6548a');
