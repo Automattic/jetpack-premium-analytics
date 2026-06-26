ded89c1cb7c334ec0812a128e740c5e507665d37
kangzj <kangzj@users.noreply.github.com>
2026-06-26 03:57:01 +0000

    Premium Analytics: add Stats app referrers spam endpoint (#49893)
    * Premium Analytics: add Stats app referrers spam endpoint
    
    * Premium Analytics: type referrers spam endpoint
    
    * Premium Analytics: remove referrers spam hook test
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28215611332
    
    Upstream-Ref: Automattic/jetpack@37c4bf48903adcd933efbfdc236bf02579b74aee

--- build/modules/data/index.min.asset.php
+++ build/modules/data/index.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-core-data', 'wp-data', 'wp-i18n', 'wp-url'), 'version' => '782913a9e8340c2c3b99');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-core-data', 'wp-data', 'wp-i18n', 'wp-url'), 'version' => '7dc4ad2023dd3c3a1c28');
