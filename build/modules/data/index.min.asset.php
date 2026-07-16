4ba104ec907f6f80518dac9a5da7c0762fbd6546
anomiex <anomiex@users.noreply.github.com>
2026-07-16 20:25:50 +0000

    Update tanstack-query monorepo to v5.101.2 (#50582)
    * Update tanstack-query monorepo to v5.101.2
    * Fix pnpmfile
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29530903089
    
    Upstream-Ref: Automattic/jetpack@9492e33d5997079ae631f38c5ce73df2511d911a

--- build/modules/data/index.min.asset.php
+++ build/modules/data/index.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-core-data', 'wp-data', 'wp-html-entities', 'wp-i18n', 'wp-url'), 'version' => '7c6c0e1bee6195b44f82');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-core-data', 'wp-data', 'wp-html-entities', 'wp-i18n', 'wp-url'), 'version' => 'b31a89e70c7875c10846');
