1c95bde904cd6c5668efd501f010dc362ae73e7e
simison <simison@users.noreply.github.com>
2026-07-17 08:23:42 +0000

    Newsletters: add WPDS fallbacks during build (#50583)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29565395993
    
    Upstream-Ref: Automattic/jetpack@c6bbd113fa551855419dac84c38cced1e8cba3fe

--- build/modules/data/index.min.asset.php
+++ build/modules/data/index.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-core-data', 'wp-data', 'wp-html-entities', 'wp-i18n', 'wp-url'), 'version' => '7d0d6f9afa412e833333');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-core-data', 'wp-data', 'wp-html-entities', 'wp-i18n', 'wp-url'), 'version' => '4fad417ee5fbc5cad542');
